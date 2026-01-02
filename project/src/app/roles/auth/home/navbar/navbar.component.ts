4d5117742623e49487510499472e197956ab2607
Umesh <umeshkargvel@gmail.com>
2026-01-02 15:53:05 +0530

    created navbar

--- project/src/app/roles/auth/home/navbar/navbar.component.ts
+++ project/src/app/roles/auth/home/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
@@ -7,5 +7,11 @@ import { Component } from '@angular/core';
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
+isScrolled = false;
 
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    // Change background after 50px of scrolling
+    this.isScrolled = window.scrollY > 50;
+  }
 }
