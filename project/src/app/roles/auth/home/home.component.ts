bdf5df1d231104954c87346c4a376100d135d306
kobi <krishn@gmail>
2026-01-02 16:29:02 +0530

    features added

--- project/src/app/roles/auth/home/home.component.ts
+++ project/src/app/roles/auth/home/home.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from './navbar/navbar.component';
+import { FeaturesComponent } from './features/features.component';
 
 @Component({
   selector: 'app-home',
-  imports: [NavbarComponent],
+  imports: [NavbarComponent, FeaturesComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
